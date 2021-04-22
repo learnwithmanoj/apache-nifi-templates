71b417b60b5f0e2d5415ecff5db75da3abd3d7f6
Manoj G T <gtmanoj235@gmail.com>
2021-04-22 10:33:58 +0530

    Added Hive JDBC Example

---
+++ Hive Examples/sql/db-script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS employee ( eid int, name String,
+salary String, destination String)
+COMMENT 'Employee details'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE;
